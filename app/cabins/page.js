944f7a9bbdcc5d3fdf21079ef9c95ed6acb80eb2
kartick-weloin <kartick.sadhu@weloin.com>
2024-11-24 00:25:32 +0530

    setup css styling, fonts and add page metadata

---
+++ app/cabins/page.js
@@ -0,0 +1,7 @@
+export const metadata = {
+  title: 'Cabin',
+}
+
+export default function page() {
+  return <h1>Cabins</h1>
+}
--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,35 @@
+import '@/app/_styles/globals.css'
+import Logo from '@/app/_components/Logo'
+import Navigation from '@/app/_components/Navigation'
+import { Josefin_Sans } from 'next/font/google'
+
+const josefin = Josefin_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+export const metadata = {
+  title: {
+    template: '%s | The Wild Oasis',
+    default: 'Welcome | The Wild Oasis',
+  },
+  description:
+    'Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests',
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+      >
+        <header>
+          <Logo />
+          <Navigation />
+        </header>
+        <main>{children}</main>
+        <footer>Copyright by The Wile Oasis</footer>
+      </body>
     </html>
   )
 }
