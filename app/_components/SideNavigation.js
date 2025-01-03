1278ac4f70bf729021b537ec9aeee3f2cd068fc5
kartick-weloin <kartick.sadhu@weloin.com>
2025-01-03 13:54:12 +0530

    show active side nav

--- app/_components/SideNavigation.js
+++ app/_components/SideNavigation.js
@@ -1,5 +1,8 @@
-import { CalendarDaysIcon, HomeIcon, UserIcon } from '@heroicons/react/24/solid'
+'use client'
+
 import SignOutButton from './SignOutButton'
+import { usePathname } from 'next/navigation'
+import { CalendarDaysIcon, HomeIcon, UserIcon } from '@heroicons/react/24/solid'
 
 const navLinks = [
   {
@@ -20,13 +23,17 @@ const navLinks = [
 ]
 
 function SideNavigation() {
+  const pathname = usePathname()
+
   return (
     <nav className="border-r border-primary-900">
       <ul className="flex flex-col gap-2 h-full text-lg">
         {navLinks.map((link) => (
           <li key={link.name}>
             <a
-              className={`py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200`}
+              className={`py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200 ${
+                pathname === link.href ? 'bg-primary-900' : ''
+              }`}
               href={link.href}
             >
               {link.icon}
--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,9 +1,12 @@
+import TextExpander from '@/app/_components/TextExpander'
 import { getCabin, getCabins } from '@/app/_lib/data-service'
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from '@heroicons/react/24/solid'
 import Image from 'next/image'
 
 export async function generateMetadata({ params }) {
-  const { name } = await getCabin(params.cabinId)
+  const { cabinId } = await params
+
+  const { name } = await getCabin(cabinId)
 
   return { title: `Cabin ${name}` }
 }
@@ -12,14 +15,18 @@ export async function generateStaticParams() {
   const cabins = await getCabins()
 
   const ids = cabins.map((cabin) => {
-    cabinId: String(cabin.id)
+    return {
+      cabinId: String(cabin.id),
+    }
   })
 
   return ids
 }
 
 export default async function Page({ params }) {
-  const cabin = await getCabin(params?.cabinId)
+  const { cabinId } = await params
+
+  const cabin = await getCabin(cabinId)
 
   const { id, name, maxCapacity, regularPrice, discount, image, description } =
     cabin
@@ -41,7 +48,9 @@ export default async function Page({ params }) {
             Cabin {name}
           </h3>
 
-          <p className="text-lg text-primary-300 mb-10">{description}</p>
+          <p className="text-lg text-primary-300 mb-10">
+            <TextExpander>{description}</TextExpander>
+          </p>
 
           <ul className="flex flex-col gap-4 mb-7">
             <li className="flex gap-3 items-center">
