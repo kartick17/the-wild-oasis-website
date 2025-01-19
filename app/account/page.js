459c2f826fe641cee24f25828088773c4ea7734c
kartick17 <karticksadhu0@gmail.com>
2025-01-19 13:54:10 +0530

    implement signin and signout

--- app/account/page.js
+++ app/account/page.js
@@ -1,11 +1,16 @@
+import { auth } from '../_lib/auth'
+
 export const metadata = {
   title: 'Guest area',
 }
 
-export default function page() {
+export default async function page() {
+  const { user } = await auth()
+  const firstName = user?.name?.split(' ').at(0)
+
   return (
-    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-      Welcome, User
+    <h2 className='font-semibold text-2xl text-accent-400 mb-7'>
+      Welcome, {firstName}
     </h2>
   )
 }
